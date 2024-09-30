144d386a4eb4689a971d92cb2c75365c623a0b7c
nicostrada <nicolas.estrada90@gmail.com>
2024-09-30 22:34:34 +0200

    Working

--- myfirstprogram.c
+++ myfirstprogram.c
@@ -1,7 +1,31 @@
 # include <stdio.h>
 
 int main() {
-    printf("Hello World!");
+    printf("Hello World!\n\n");
     printf("Have a good day!");
+    /* printf("Have a good day!"); */
+
+    int myNum = 15;
+    float myFloatNum = 5.99;
+    char myLetter = 'D';
+
+    printf("%d\n",myNum);
+    printf("%f\n", myFloatNum);
+    printf("%c\n",myLetter);
+    printf("\n");
+
+    int x = 5, y = 6, z = 50;
+    printf("%d", x+y+z);
+
+/* 
+The general rules for naming variables are:
+*    Names can contain letters, digits and underscores
+*    Names must begin with a letter or an underscore (_)
+*    Names are case-sensitive (myVar and myvar are different variables)
+*    Names cannot contain whitespaces or special characters like !, #, %, etc.
+*    Reserved words (such as int) cannot be used as names 
+*/
+
+
     return 0;
 }
